a5666fa68e46ddf90e13d71aed0f52032377b303
Sajal Arora <sajalarora007@gmail.com>
2018-04-27 15:18:14 +0530

    This is the first and probably the last commit for this build.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer : AVAudioPlayer!
+    var soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var selectedSound: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,9 +22,19 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        selectedSound = soundArray[sender.tag-1]
+        playSound()
+    }
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSound, withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+
     }
     
   
